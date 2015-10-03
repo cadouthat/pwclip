5596c23c9c2917a58ec83b25a5ba59df460accb4
Connor Douthat <connor@steelcastleweb.com>
2015-10-02 22:33:47 -0700

    Windows clipboard helpers

--- core/clipHelp.h
+++ core/clipHelp.h
@@ -5,6 +5,24 @@ by: Connor Douthat
 */
 bool WipeClipboard()
 {
-	//
-	return false;
+	if(!OpenClipboard(NULL)) return false;
+	bool foundAndErased = false;
+	HANDLE hClip = GetClipboardData(CF_TEXT);
+	if(hClip)
+	{
+		LPVOID clipData = GlobalLock(hClip);
+		if(clipData)
+		{
+			SIZE_T clipSize = GlobalSize(hClip);
+			if(clipSize)
+			{
+				memset(clipData, 0, clipSize);
+				foundAndErased = true;
+			}
+			GlobalUnlock(hClip);
+		}
+		EmptyClipboard();
+	}
+	CloseClipboard();
+	return foundAndErased;
 }
