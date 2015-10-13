6975fd2ee2172947337317d5bb6195fc2d950696
Connor Douthat <connor@steelcastleweb.com>
2015-10-12 17:44:24 -0700

    Reliable decryption verification

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 Core
 ====
-* Reliably determine encryption success
 * Change the replace action to a flag
 * Command to generate random password
 * Command to export raw passwords to a backup file
--- core/crypto/PasswordCipher.h
+++ core/crypto/PasswordCipher.h
@@ -3,23 +3,13 @@ Symmetric cryptography with password-based key
 by: Connor Douthat
 10/4/2015
 */
+#define CRYPTO_VERIFY_PREFIX "/Plaintext warning!/"
+
 class PasswordCipher
 {
 	unsigned char key[CRYPTO_KEY_SIZE];
 
-public:
-	PasswordCipher(const char *pass)
-	{
-		memset(key, 0, sizeof(key));
-		//Generate key via PBKDF2-SHA256, with no salt (we need the same key every time)
-		PKCS5_PBKDF2_HMAC(pass, -1, NULL, 0, CRYPTO_ITERATIONS, EVP_sha256(), sizeof(key), key);
-	}
-	~PasswordCipher()
-	{
-		//Wipe key
-		memset(key, 0, sizeof(key));
-	}
-	char *encrypt(const char *plain, unsigned char *iv_out)
+	char *raw_encrypt(const char *plain, unsigned char *iv_out)
 	{
 		//Generate IV
 		if(!RAND_bytes(iv_out, CRYPTO_BLOCK_SIZE)) return NULL;
@@ -46,7 +36,7 @@ public:
 		EVP_CIPHER_CTX_free(ctx);
 		return enc;
 	}
-	char *decrypt(const char *enc, unsigned char *iv)
+	char *raw_decrypt(const char *enc, unsigned char *iv)
 	{
 		unsigned char *plain = NULL;
 		EVP_CIPHER_CTX *ctx;
@@ -79,5 +69,54 @@ public:
 		}
 		EVP_CIPHER_CTX_free(ctx);
 		return (char*)plain;
+	}
+
+public:
+	PasswordCipher(const char *pass)
+	{
+		memset(key, 0, sizeof(key));
+		//Generate key via PBKDF2-SHA256, with no salt (we need the same key every time)
+		PKCS5_PBKDF2_HMAC(pass, -1, NULL, 0, CRYPTO_ITERATIONS, EVP_sha256(), sizeof(key), key);
+	}
+	~PasswordCipher()
+	{
+		//Wipe key
+		memset(key, 0, sizeof(key));
+	}
+	char *encrypt(const char *plain, unsigned char *iv_out)
+	{
+		//Allocate space for prefixed string
+		int prefix_len = strlen(CRYPTO_VERIFY_PREFIX);
+		int plain_len = strlen(plain);
+		char *prefixed = (char*)malloc(prefix_len + plain_len + 1);
+		//Copy prefix and plaintext
+		strcpy(prefixed, CRYPTO_VERIFY_PREFIX);
+		strcat(prefixed, plain);
+		//Encrypt prefixed string
+		char *enc = raw_encrypt(prefixed, iv_out);
+		//Wipe and free prefixed string
+		memset(prefixed, 0, prefix_len + plain_len);
+		free(prefixed);
+		return enc;
+	}
+	char *decrypt(const char *enc, unsigned char *iv)
+	{
+		//Decrypt prefixed string
+		char *prefixed = raw_decrypt(enc, iv);
+		if(!prefixed) return NULL;
+		//Verify correct prefix
+		int prefix_len = strlen(CRYPTO_VERIFY_PREFIX);
+		int plain_len = strlen(prefixed) - prefix_len;
+		char *plain = NULL;
+		if(plain_len > 0 && !memcmp(prefixed, CRYPTO_VERIFY_PREFIX, prefix_len))
+		{
+			//Allocate and extract plaintext
+			plain = (char*)malloc(plain_len + 1);
+			strcpy(plain, prefixed + prefix_len);
+		}
+		//Wipe and free prefixed string
+		memset(prefixed, 0, prefix_len + plain_len);
+		free(prefixed);
+		return plain;
 	}
 };
